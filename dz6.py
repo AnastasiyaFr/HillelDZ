fa12b6f614ed190418a3cc889ae30e82cc4ec859
anast <nastya.friday"gmail.com>
2021-02-05 20:21:02 +0200

    corrections on Vova comments

--- dz6.py
+++ dz6.py
@@ -27,18 +27,23 @@ my_result2=[value for value in my_list2 if value[0]=="a" ]
 print(my_result2)
 # 3. Дан список строк my_list. Создать новый список в который поместить
 # элементы из my_list в которых есть символ - буква "a" на любом месте.
-# Вариант 1
+# # Вариант 1
+# my_list3=["fgh","abn","fajkli","agh","babn","fjali"]
+# my_result3=[]
+# for symbol in my_list3:
+#     for symbol1 in symbol:
+#         if symbol1=="a":
+#             my_result3.append(symbol)
+# print(my_result3)
+# # Вариант 2
+# my_list3=["fgh","abn","fajkli","agh","babn","fjali"]
+# my_result3=[value for value in my_list3 if value.rfind("a")>=0]
+# print(my_result3)
+# Вариант 3 c in
 my_list3=["fgh","abn","fajkli","agh","babn","fjali"]
-my_result3=[]
-for symbol in my_list3:
-    for symbol1 in symbol:
-        if symbol1=="a":
-            my_result3.append(symbol)
+my_result3=[value for value in my_list3 if "a" in value]
 print(my_result3)
-# Вариант 2
-my_list3=["fgh","abn","fajkli","agh","babn","fjali"]
-my_result3=[value for value in my_list3 if value.rfind("a")>=0]
-print(my_result3)
+
 # 4. Дан список my_list в котором могум быть как строки (type str) так и целые числа (type int).
 # Создать новый список в который поместить только строки из my_list.
 # Вариант 1
@@ -55,7 +60,8 @@ print(my_result4)
 # 5. Дана строка my_str. Создать список в который поместить те символы из my_str,
 # которые встречаются в строке только один раз.
 my_str5="wsaafddff"
-my_result5=[value for value in my_str5 if my_str5.count(value)==1]
+setmystr5=set(my_str5)
+my_result5=[value for value in setmystr5 if my_str5.count(value)==1]
 print(my_result5)
 # 6. Даны две строки. Создать список в который поместить те символы,
 # которые есть в обеих строках хотя бы раз.
@@ -77,6 +83,12 @@ my_str7_1="wsaafddffhjk"
 my_str7_2="wsaafddff"
 my_result7=[value for value in my_str7_1 if my_str7_2.count(value)==1 and my_str7_1.count(value)==1]
 print(set(my_result7))
+#Не уверена что Вы это имели ввиду
+my_str7_1="wsaafddffhjk"
+my_str7_2="wsaafddff"
+my_str7set=set(my_str7_1).intersection(set(my_str7_2))
+my_result7=[value for value in my_str7set if (my_str7_2+my_str7_1).count(value)==2]
+print(my_result7)
 
 # 8. Описать с помощью словаря следующую структуру для конкретного человека (можно придумать):
 # Фамилия
